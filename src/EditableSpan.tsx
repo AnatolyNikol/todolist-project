f20e2754d1a2a770ccbaba63f1a2bad2a58bf837
Anatoly Nikolenko <fanka4ectba@mail.ru>
2024-09-22 11:48:14 +0500

    feat: add prettier and eslint settings

--- src/EditableSpan.tsx
+++ src/EditableSpan.tsx
@@ -1,37 +1,44 @@
-import React, {ChangeEvent, useState} from "react";
-import {TextField} from "@mui/material";
+import React, { ChangeEvent, useState } from 'react';
+import { TextField } from '@mui/material';
 
 type Props = {
-    value: string
-    onChange: (newTitle: string) => void
-}
+  value: string;
+  onChange: (newTitle: string) => void;
+};
 
 export const EditableSpan = (props: Props) => {
-    const {value, onChange} = props;
+  const { value, onChange } = props;
 
-    const [editMode, setEditMode] = useState(false);
-    const [title, setTitle] = useState(value)
+  const [editMode, setEditMode] = useState(false);
+  const [title, setTitle] = useState(value);
 
-    const activateEditModeHandler = () => {
-        setEditMode(true)
-    }
+  const activateEditModeHandler = () => {
+    setEditMode(true);
+  };
 
-    const deactivateEditModeHandler = () => {
-        setEditMode(false)
-    }
+  const deactivateEditModeHandler = () => {
+    setEditMode(false);
+  };
 
-    const changeTitleHandler = (event: ChangeEvent<HTMLInputElement>) => {
-        setTitle(event.currentTarget.value)
-        onChange(title)
-    }
+  const changeTitleHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.currentTarget.value);
+    onChange(title);
+  };
 
-    return (
-        <>
-            {editMode
-                ? <TextField variant={'outlined'} size={'small'} value={title} autoFocus onBlur={deactivateEditModeHandler} onChange={changeTitleHandler}/>
-                : <span onDoubleClick={activateEditModeHandler}>{value}</span>
-            }
-        </>
-
-    )
-}
+  return (
+    <>
+      {editMode ? (
+        <TextField
+          variant={'outlined'}
+          size={'small'}
+          value={title}
+          autoFocus
+          onBlur={deactivateEditModeHandler}
+          onChange={changeTitleHandler}
+        />
+      ) : (
+        <span onDoubleClick={activateEditModeHandler}>{value}</span>
+      )}
+    </>
+  );
+};
